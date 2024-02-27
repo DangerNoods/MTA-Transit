478eaf8f590bfaabd4c87e9dc77349663b8201c8
alphajames258 <james.lee4988@gmail.com>
2024-02-27 15:48:47 -0500

    hello

--- src/App.js
+++ src/App.js
@@ -4,32 +4,22 @@ import { useState, useEffect } from 'react';
 
 const App = () => {
   const SubwayContainer = (props) => {
-
     const subwayLines = [];
 
-    for (let i = 0; i < props.trainLine.length; i++){
+    for (let i = 0; i < props.trainLine.length; i++) {
       subwayLines.push(
         <SubwayLineComponent
-        color={props.color[i]}
-        trainLine={props.trainLine[i]}
-        trainStatus={props.trainStatus[i]}
-        start={props.start[i]}
-        end={props.end[i]}
+          color={props.color[i]}
+          trainLine={props.trainLine[i]}
+          trainStatus={props.trainStatus[i]}
+          start={props.start[i]}
+          end={props.end[i]}
         />
-      )
+      );
     }
-    
-    // for (let i = 0; i < props.trainLine.length; i++) {
-    //   subwayLines.push(
-    //     <SubwayLineComponent
-    //       bgColor={props.bgColor[i]}
-    //       trainLine={props.trainLine[i]}
-    //     />
-    //   );
-    // }
 
     return (
-      <div className='SubwayContainer'>
+      <div className="SubwayContainer">
         Subway Lines{' '}
         {/* <SubwayLineComponent
           bgColor={props.bgColor}
@@ -41,176 +31,107 @@ const App = () => {
     );
   };
 
-  const [trainLine, setTrainLine] = useState([])
-  const [trainStatus, setTrainStatus] = useState([])
-  const [start, setStart] = useState([])
-  const [end, setEnd] = useState([])
-  const [color, setColor] = useState([])
+  const [trainLine, setTrainLine] = useState([]);
+  const [trainStatus, setTrainStatus] = useState([]);
+  const [start, setStart] = useState([]);
+  const [end, setEnd] = useState([]);
+  const [color, setColor] = useState([]);
 
+  useEffect(() => {
+    fetchData();
+  }, []);
 
+  const fetchData = async () => {
+    try {
+      const response = await fetch('/subway');
+      const result = await response.json();
 
+      const trainLines = [];
+      const trainStatuses = [];
+      const trainStart = [];
+      const trainEnd = [];
+      const bgColor = [];
 
-  // useEffect (()=> {
-  //   const result = fetch('/subway', { method: 'GET' })
-  //   .then((response) => {
-  //     return response.json();
-  //   })
-  //   .then((response) => {
-  //     return response;
-  //   })
-  //   .catch((err) => {
-  //     console.log('error');
-  //   });
-  // }, [])
-
-useEffect (() => {
-  fetchData();
-}, [])
-
-const fetchData = async () => {
-  try {
-    const response = await fetch('/subway')
-    const result = await response.json()
-    
-    const trainLines = [];
-    const trainStatuses = []
-    const trainStart = []
-    const trainEnd = []
-    const bgColor = [];
-    
-  
-    for(let i = 0; i < result.length; i++){
-      let empty = true;
-      for (let j = 1; j < result[i].length; j++){
-        empty = false;
-        trainLines.push(result[i][0]);
-        trainStatuses.push(result[i][j].message)
-        trainStart.push(result[i][j].start)
-        trainEnd.push(result[i][j].end)
-  
+      for (let i = 0; i < result.length; i++) {
+        let empty = true;
+        for (let j = 1; j < result[i].length; j++) {
+          empty = false;
+          trainLines.push(result[i][0]);
+          trainStatuses.push(result[i][j].message);
+          trainStart.push(result[i][j].start);
+          trainEnd.push(result[i][j].end);
+        }
+        if (empty) {
+          trainLines.push(result[i][0]);
+          trainStatuses.push('Good');
+          trainStart.push('N/A');
+          trainEnd.push('N/A');
+        }
       }
-      if(empty){
-        trainLines.push(result[i][0]);
-        trainStatuses.push('Good')
-        trainStart.push('N/A')
-        trainEnd.push('N/A')
 
-      }
+      setColor(bgColor);
+      setTrainLine(trainLines);
+      setTrainStatus(trainStatuses);
+      setStart(trainStart);
+      setEnd(trainEnd);
+    } catch (error) {
+      console.log('error');
     }
-  
-    // console.log("TrainLines for Colors", trainLines)
+  };
 
-  for(let i = 0; i < trainLines.length; i++){
-    let currentTrain = trainLines[i];
-    switch (currentTrain) {
-      case '1':
-        bgColor.push('red');
-        break;
-      case '2':
-        bgColor.push('red');
-        break;
-      case '3':
-        bgColor.push('red');
-        break;
-      case '4':
-        bgColor.push('green');
-        break;
-      case '5':
-        bgColor.push('green');
-        break;
-      case '6':
-        bgColor.push('green');
-        break;
-      case 'N':
-        bgColor.push('yellow');
-        break;
-      case 'Q':
-        bgColor.push('yellow');
-        break;
-      case 'R':
-        bgColor.push('yellow');
-        break;
-      case 'W':
-        bgColor.push('yellow');
-        break;
-      case 'B':
-        bgColor.push('orange');
-        break;
-      case 'D':
-        bgColor.push('orange');
-        break;
-      case 'F':
-        bgColor.push('orange');
-        break;
-      case 'M':
-        bgColor.push('orange');
-        break;
-      case 'A':
-        bgColor.push('blue');
-        break;
-      case 'C':
-        bgColor.push('blue');
-        break;
-      case 'E':
-        bgColor.push('blue');
-        break;
-      case 'G':
-        bgColor.push('lightgreen');
-        break;
-      case 'L':
-        bgColor.push('gray');
-        break;
-      case 'J':
-        bgColor.push('brown');
-        break;
-      case 'Z':
-        bgColor.push('brown');
-        break;
-      case '7':
-        bgColor.push('purple');
-        break;
-    }
-  }
+  const handleClick = () => {
+    let filteredTrainLine = [];
+    let filteredTrainStatus = [];
+    let filteredStart = [];
+    let filteredEnd = [];
 
-  console.log(bgColor)
+    // for (let i = 0; i < trainLine.length; i++) {
+    //   if (i === 0 || i === 1 || i === 2) {
+    //     filteredTrainLine.push(trainLine[i]);
+    //     filteredTrainStatus.push(trainStatus[i]);
+    //     filteredStart.push(start[i]);
+    //     filteredEnd.push(end[i]);
+    //   } else {
+    //     break;
+    //   }
+     
+     
 
-    setColor(bgColor)
-    setTrainLine(trainLines)
-    setTrainStatus(trainStatuses)
-    setStart(trainStart)
-    setEnd(trainEnd)
+      
+      
+    //   console.log(filteredTrainLine)
+    //   console.log(filteredTrainStatus)
+    //   console.log(filteredStart)
+    //   console.log(filteredEnd, 'filteredEnd')
 
+    // }
+     filteredTrainLine = trainLine.slice(0, 3);
+     filteredTrainStatus = trainStatus.slice(0, 3);
+     filteredStart = start.slice(0, 3);
+     filteredEnd = end.slice(0, 3);
+     
 
-  } catch (error) {
-    console.log("error")
-  }
-}
-
-
-// console.log(object)
-// console.log('abc', Object.keys(object))
-// let trainStatus = trainInfo[1].message
-
-
-  // const lines = result[0];
-  // const colors = [
-  //   'red',
-  //   'green',
-  //   'purple',
-  //   'blue',
-  //   'orange',
-  //   'lightgreen',
-  //   'gray',
-  //   'brown',
-  //   'yellow',
-  // ];
-
-  // console.log(bgColor);
+  };
 
   return (
     <div>
       <h1>Subway Alerts</h1>
-      <SubwayContainer color={color} trainLine={trainLine} trainStatus={trainStatus} start={start} end={end}/>
+      <button onClick={handleClick()}>train (1,2,3)</button>
+      <button>train(4,5,6)</button>
+      <button>train 7</button>
+      <button>train (A,C,E)</button>
+      <button>train (B,D,F,M)</button>
+      <button>train (G)</button>
+      <button>train (L)</button>
+      <button>train (N,G,R,W)</button>
+
+      <SubwayContainer
+        color={color}
+        trainLine={trainLine}
+        trainStatus={trainStatus}
+        start={start}
+        end={end}
+      />
       <div></div>
     </div>
   );
