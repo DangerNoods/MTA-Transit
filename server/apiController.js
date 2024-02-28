0695a37470290b5b90a3fa67257aa83f8870fc69
d.wong92 <d.wong92@gmail.com>
2024-02-27 20:04:37 -0500

    fetching data from xml feed

--- server/apiController.js
+++ server/apiController.js
@@ -1,67 +1,111 @@
-const GtfsRealtimeBindings = require('gtfs-realtime-bindings');
+const { text } = require("body-parser");
+const GtfsRealtimeBindings = require("gtfs-realtime-bindings");
 
 const URL =
-  'https://api-endpoint.mta.info/Dataservice/mtagtfsfeeds/camsys%2Fsubway-alerts';
-const key = 'IWddVccjk6aIOEuV87rGV8p2551sWLzI3y6O65yu';
+  "https://api-endpoint.mta.info/Dataservice/mtagtfsfeeds/camsys%2Fsubway-alerts";
+const URL2 =
+  "https://api-endpoint.mta.info/Dataservice/mtagtfsfeeds/nyct%2Fnyct_ene.xml";
+const key = "IWddVccjk6aIOEuV87rGV8p2551sWLzI3y6O65yu";
 
 const ApiController = {
+  getAccInfo(req, res, next) {
+    fetch(URL2, {
+      headers: { "x-api-key": key },
+    })
+      .then((response) => {
+        return response.text();
+      })
+      .then((response) => {
+        //DOM parser
+        console.log('Second then res ', response)
+        const parser = new DOMParser(); //parsing data to new Dom
+        const doc = parser.parseFromString(response, "text/xml");
+
+        //navigate and query this document using standard DOM methods
+
+        return next();
+      })
+      .catch((err) =>
+        next({
+          log: "Express error handler caught accessibility error",
+          status: 500,
+          message: { err: "An error occurred" },
+        })
+      );
+  },
+
   async getSubwayInfo(req, res, next) {
     try {
       response = await fetch(URL, {
-        headers: { 'x-api-key': key },
+        headers: { "x-api-key": key },
       });
-      
+
       const buffer = await response.arrayBuffer();
       const feed = GtfsRealtimeBindings.transit_realtime.FeedMessage.decode(
         new Uint8Array(buffer)
       );
-      
-      console.log(feed)
 
-      // const currentTime = Math.floor(Date.now() / 1000);
-      // const routes = ["1","2", "3", "4", "5", "6", "7", "A", "C", "E", "B", "D", "F", "M", "G", "L", "N", "Q", "R", "W" ];
-      // const data = [];
+      const currentTime = Math.floor(Date.now() / 1000);
+      const routes = [
+        "1",
+        "2",
+        "3",
+        "4",
+        "5",
+        "6",
+        "7",
+        "A",
+        "C",
+        "E",
+        "B",
+        "D",
+        "F",
+        "M",
+        "G",
+        "L",
+        "N",
+        "Q",
+        "R",
+        "W",
+      ];
+      const data = [];
 
-      // routes.forEach((route) => {
-      //   const info = [route];
-      //   const cache = {};
+      routes.forEach((route) => {
+        const info = [route];
+        const cache = {};
 
-      //   feed.entity.forEach((element) => {
-      //     element.alert.informedEntity.forEach((x) => {
+        feed.entity.forEach((element) => {
+          element.alert.informedEntity.forEach((x) => {
+            if (x.routeId === route) {
+              element.alert.activePeriod.forEach((y) => {
+                if (y.start.low < currentTime) {
+                  if (y.end.low === 0 || y.end.low > currentTime) {
+                    const message =
+                      element.alert.headerText.translation[0].text;
+                    const start = new Date(y.start.low * 1000);
+                    const end = new Date(y.end.low * 1000);
 
-      //       if (x.routeId === route) {
-      //         element.alert.activePeriod.forEach((y) => {
+                    if (!cache[message]) {
+                      const obj = {
+                        message: message,
+                        start: dateString(start),
+                      };
+                      if (y.end.low !== 0) obj.end = dateString(end);
+                      else obj.end = "Unknown";
+                      info.push(obj);
+                      cache[message] = true;
+                    }
+                  }
+                }
+              });
+            }
+          });
+        });
+        data.push(info);
+      });
 
-      //           if (y.start.low < currentTime) {
-      //             if (y.end.low === 0 || y.end.low > currentTime) {
-
-      //               const message =
-      //                 element.alert.headerText.translation[0].text;
-      //               const start = new Date(y.start.low * 1000);
-      //               const end = new Date(y.end.low * 1000);
-
-      //               if (!cache[message]) {
-      //                 const obj = {
-      //                   message: message,
-      //                   start: dateString(start),
-      //                 };
-      //                 if (y.end.low !== 0) obj.end = dateString(end);
-      //                 else obj.end = 'Unknown';
-      //                 info.push(obj);
-      //                 cache[message] = true;
-      //               }
-      //             }
-      //           }
-      //         });
-      //       }
-      //     });
-      //   });
-      //   data.push(info);
-      // });
-
-      res.locals.data = feed;
+      res.locals.data = data;
       return next();
-      
     } catch {
       return next((error) => console.log(`Error fetching data`, error));
     }
@@ -85,8 +129,8 @@ function dateString(date) {
   if (minutes < 10) string += `0${minutes}`;
   else string += `${minutes}`;
 
-  if (pm) string += ' PM';
-  else string += ' AM';
+  if (pm) string += " PM";
+  else string += " AM";
 
   return string;
 }
