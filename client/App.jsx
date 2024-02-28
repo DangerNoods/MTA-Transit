2c91366c85bf73e231b290a4c304212d60137fa4
d.wong92 <d.wong92@gmail.com>
2024-02-28 17:22:17 -0500

    front end for accessiblity. Partially styling.

--- client/App.jsx
+++ client/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import MainContainer from './container/MainContainer.jsx';
 import TrainGroupContainer from './container/TrainGroupContainer.jsx';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import AccessibilityContainer from './container/AccessibilityContainer.jsx';
 
 const App = () => {
   return (
@@ -10,6 +11,7 @@ const App = () => {
         <Routes>
           <Route path="/" element={<MainContainer />} />
           <Route path="/:currTrainGroup" element={<TrainGroupContainer />} />
+          <Route path="/abc" element={<AccessibilityContainer />} />
         </Routes>
       </Router>
     </div>
---
+++ client/component/AccessiblityComponent.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+const AccessibilityComponent = (props) => {
+
+    return (
+        <div className="accessibilityTotalComponent">
+          
+          <div className="station">
+            <p>{props.station}</p>
+          </div>
+
+          <div className="trainNo">
+            <p> Trainlines: {props.trainNo}</p>
+          </div>
+          
+          <div className="outageDates">
+            <p>Outage Dates: {props.outageDates}</p>
+          </div>
+          
+          <div className="estimatedReturntoService">
+            <p>Est. Return to Service: {props.estimatedReturntoService}</p>
+          </div>
+
+          <div className="ADA">
+            <p> Handicap Accessible: {props.ADA}</p>
+          </div>
+
+        </div>
+    )
+
+}
+
+export default AccessibilityComponent;
